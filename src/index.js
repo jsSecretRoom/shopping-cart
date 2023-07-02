436150552cf8fd207ff3751cf8f494f953e79442
Rus1anGU <95142486+Rus1anGU@users.noreply.github.com>
2023-07-02 17:51:37 +0300

    v3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,57 @@
-import { data } from './js/data';
-
 document.addEventListener('DOMContentLoaded', function() {
-  function mimiShopModul(data, smartfonsSelector){
-
-    const createCaart = document.querySelector(smartfonsSelector);
-
+  function myCards(objdata, typeOfProduct, typeSelector ){
     class Product {
-      constructor(name, description, fotoLink, firstPrice, secondPrice){
+      constructor(name, description, fotoLink, firstPrice, secondPrice, parentSelector){
         this.name = name;
         this.description = description;
         this.fotoLink = fotoLink;
         this.firstPrice = firstPrice;
         this.secondPrice = secondPrice;
+        this.parent = document.querySelector(parentSelector);
       }
-       
-      render(){
         
+      render(){
+        const element = document.createElement('div');
+        element.innerHTML = `
+        <div class="card">
+          <div class="ficha">
+              <button class="add-to-bag">
+                  <img src="./src/img/Favorite.svg" alt="">
+              </button>
+          </div>
+          <div class="grup-card">
+              <div class="product-img">
+                  <img src="${this.fotoLink}" alt="">
+              </div>
+              <a href="#" class="product-info">
+                  <p class="product-desck"><span class="product">${this.name}</span>${this.description}</p>
+                  <p class="unreal-price">${this.firstPrice}</p>
+                  <p class="real-price">${this.secondPrice}</p>
+              </a>
+          </div>
+        </div>`
         this.parent.append(element);
       }
-      
     }
+    
+    fetch(objdata)
+      .then(response => response.json())
+      .then(data => {
+        const cardsData = data[typeOfProduct];
+        cardsData.forEach(({ name, description, fotoLink, firstPrice, secondPrice }) => {
+          new Product(name, description, fotoLink, firstPrice, secondPrice, typeSelector).render();
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }
-  mimiShopModul(data, '.smartfons');
+
+  myCards('data.json','smartphones', '#smartfons' );
+  myCards('data.json','smartphones', '#cars' );
+  myCards('data.json','smartphones', '#pc' );
+  myCards('data.json','smartphones', '#tables' );
+  myCards('data.json','smartphones', '#soffa' );
+  myCards('data.json','smartphones', '#house' );
 });
+
--- src/js/data.js
+++
@@ -1,9 +0,0 @@
-[
-    {
-        name : 'aifonpromax',
-        description: 'wery good aifon',
-        fotoLink: 'http://',
-        firstPrice : '8888',
-        secondPrice : '9999'
-    }
-]
